de7ae12bc0f3f82d22133a9f4b5a01e88dc0a070
Elson Li <elson.li@berkeley.edu>
2020-02-23 18:51:02 -0800

    dist subtree commit

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-// Game board is 10 cols x 20 rows, and each square is 40px x 40px
-export const NUM_COLS = 10;
-export const NUM_ROWS = 20;
-export const TILE_SIZE = 40;
-export const DIM_X = 400;
-export const DIM_Y = 800;
-export const BG_COLOR = "#111111";
